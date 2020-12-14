3afb0ce27d1ba742cf6d6fdae723e2ae78a6f12f
Givranuke <43473001+Givranuke@users.noreply.github.com>
2020-12-14 16:52:14 +0100

    1 test

---
+++ src/test/java/com/automation/selenium/matheo/FonctionMethods.java
@@ -0,0 +1,81 @@
+package com.automation.selenium.matheo;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.interactions.Actions;
+
+public class FonctionMethods {
+
+	public static void login(WebDriver driver, String email, String password) throws InterruptedException {
+		WebElement vSignInButton = driver.findElement(ByXPath.xpath("/html/body/div[1]/div/div/div/div/button"));
+		vSignInButton.click();
+		
+		String mainWindow = driver.getWindowHandle();
+		
+		Set<String> windows = driver.getWindowHandles();
+		Iterator<String> iterator = windows.iterator();
+		
+		while(iterator.hasNext()) {
+			String childWindow = iterator.next();
+			
+			if(!mainWindow.equalsIgnoreCase(childWindow)) {
+				driver.switchTo().window(childWindow);
+				Thread.sleep(5000);
+				WebElement emailField = driver.findElement(By.name("username"));
+				emailField.sendKeys(email);
+				Thread.sleep(500);
+				WebElement passwordField = driver.findElement(By.name("password"));
+				passwordField.sendKeys(password);
+				Thread.sleep(500);
+				WebElement vSignButton = driver.findElement(By.name("commit"));
+				vSignButton.click();
+				
+				WebElement vAccordAccessButton = driver.findElement(ByXPath.xpath("/html/body/div/div[2]/div/form/input[5]"));
+				vAccordAccessButton.click();
+			}
+		}
+		
+		driver.switchTo().window(mainWindow);
+	}
+	
+	public static void testRenseignerReleveSurLaCarteAvecAjoutPhoto(WebDriver driver) throws InterruptedException {		
+		Thread.sleep(5000);
+		Actions builder = new Actions(driver);
+		builder.moveToElement(driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[1]")), 0, 0);
+		builder.moveByOffset(100, 100).click().build().perform();
+		
+		WebElement vCreatePlan = driver.findElement(ByXPath.xpath("/html/body/div[5]/div[3]/div/div[2]/button[2]"));
+		vCreatePlan.click();
+		
+		WebElement genreCommun = driver.findElement(By.id("commonGenusSelect"));
+		WebElement genreLatin = driver.findElement(By.id("GenusSelect"));
+		WebElement especeCommune = driver.findElement(By.id("commonSpeciesSelect"));
+		WebElement especeLatine = driver.findElement(By.id("SpeciesSelect"));
+		
+		genreCommun.sendKeys("Albizzia" + Keys.DOWN + Keys.ENTER);
+		genreLatin.sendKeys("Albizia" + Keys.DOWN + Keys.ENTER);
+		especeCommune.sendKeys("Albizzia commun" + Keys.DOWN + Keys.ENTER);
+		especeLatine.sendKeys("Albizia Julibrissin" + Keys.DOWN + Keys.ENTER);	
+		
+		WebElement confiant = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[5]/label[3]/span[1]"));
+		confiant.click();
+		
+		WebElement inputPicture = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[6]/div/div/input"));
+		inputPicture.sendKeys("C:/Users/Oydrey/Pictures/arbre.jpg");
+
+		WebElement valider = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/button[1]"));
+		valider.click();
+		
+		WebElement confirmValider = driver.findElement(ByXPath.xpath("/html/body/div[5]/div[3]/div/div[2]/button[2]"));
+		confirmValider.click();
+	}
+	
+}
