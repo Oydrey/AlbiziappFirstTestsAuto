4eb6542b5e9015ac5dfb59b05cce3d626fe50d77
Oydrey <oydrey@gmail.com>
2020-12-14 10:28:52 +0100

    maj

--- src/test/java/com/automation/selenium/CommonMethods.java
+++ src/test/java/com/automation/selenium/CommonMethods.java
@@ -2,53 +2,19 @@ package com.automation.selenium;
 
 import static org.junit.Assert.assertEquals;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.By.ByXPath;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.interactions.Actions;
 
-public class AlbiziappTest {
-	
-	private static final DesiredCapabilities CAPABILITY = DesiredCapabilities.chrome();
+public class CommonMethods {
 
-	private static final String SELENIUM_SERVER_URL = "http://127.0.0.1:4444/wd/hub";
-
-	private static final String LOGIN_URL = "https://albiziapp.ozytis.fr/login";
-	
-	private WebDriver mDriver = null;
-	
-	private String email = "tqlgroupe1@gmail.com";
-	
-	private String password = "tqlgroupe1";
-	
-	@Before
-	public void setup() throws MalformedURLException, InterruptedException {
-		// Create a new instance of the driver
-		mDriver = new RemoteWebDriver(new URL(SELENIUM_SERVER_URL), CAPABILITY);
-		
-		// And now use this to open base url
-		mDriver.navigate().to(LOGIN_URL);
-
-		// Ask to the driver to wait for 3s when an element is not found
-		mDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		mDriver.manage().window().maximize();
-	}
-	
-	@Test
-	public void testAuthentificationAvecIdentifiantsValides() throws InterruptedException {
+	public static void login(WebDriver mDriver, String email, String password) {
 		WebElement vSignInButton = mDriver.findElement(ByXPath.xpath("/html/body/div[1]/div/div/div/div/button"));
 		vSignInButton.click();
 		
@@ -64,15 +30,10 @@ public class AlbiziappTest {
 				mDriver.switchTo().window(childWindow);
 				
 				WebElement emailField = mDriver.findElement(By.name("username"));
-				emailField.sendKeys("tqlgroupe1@gmail.com");
+				emailField.sendKeys(email);
 				
 				WebElement passwordField = mDriver.findElement(By.name("password"));
-				passwordField.sendKeys("tqlgroupe1");
-				
-				//verif mail
-				assertEquals("tqlgroupe1@gmail.com", emailField.getAttribute("value"));
-				//verif password
-				assertEquals("tqlgroupe1", passwordField.getAttribute("value"));
+				passwordField.sendKeys(password);
 				
 				WebElement vSignButton = mDriver.findElement(By.name("commit"));
 				vSignButton.click();
@@ -85,21 +46,15 @@ public class AlbiziappTest {
 		mDriver.switchTo().window(mainWindow);
 	}
 	
-	@Test
-	public void testRenseignerReleveSurLaCarteAvecAjoutPhoto() throws InterruptedException {
-		CommonMethods.login(mDriver, email, password);
-		
-		//clique sur la carte
-		Thread.sleep(5000);		//nécessaire pour que le carte charge
+	public static void testRenseignerReleveSurLaCarteAvecAjoutPhoto(WebDriver mDriver) throws InterruptedException {		
+		Thread.sleep(5000);
 		Actions builder = new Actions(mDriver);
 		builder.moveToElement(mDriver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[1]")), 0, 0);
 		builder.moveByOffset(100, 100).click().build().perform();
 		
-		//confirme creation de relevé
 		WebElement vCreatePlan = mDriver.findElement(ByXPath.xpath("/html/body/div[5]/div[3]/div/div[2]/button[2]"));
 		vCreatePlan.click();
 		
-		//renseigne les champs
 		WebElement genreCommun = mDriver.findElement(By.id("commonGenusSelect"));
 		WebElement genreLatin = mDriver.findElement(By.id("GenusSelect"));
 		WebElement especeCommune = mDriver.findElement(By.id("commonSpeciesSelect"));
@@ -113,24 +68,9 @@ public class AlbiziappTest {
 		WebElement confiant = mDriver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[5]/label[3]/span[1]"));
 		confiant.click();
 		
-		/* Permet de cliquer sur l'appareil photo puis "Choisir une photo existante" *
-		WebElement photo = mDriver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[6]/div/div/span"));
-		photo.click();
-		
-		WebElement chosePicture = mDriver.findElement(ByXPath.xpath("/html/body/div[5]/div[3]/div/div/div/button[2]"));
-		chosePicture.click();
-		*/
-		
 		WebElement inputPicture = mDriver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[6]/div/div/input"));
 		inputPicture.sendKeys("C:/Users/Oydrey/Pictures/arbre.jpg");
-		
-		//verification avant validation
-		assertEquals("Albizzia", genreCommun.getAttribute("value"));
-		assertEquals("Albizia", genreLatin.getAttribute("value"));
-		assertEquals("Albizzia commun", especeCommune.getAttribute("value"));
-		assertEquals("Albizia Julibrissin", especeLatine.getAttribute("value"));
-		
-		//validation
+
 		WebElement valider = mDriver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/button[1]"));
 		valider.click();
 		
@@ -138,9 +78,4 @@ public class AlbiziappTest {
 		confirmValider.click();
 	}
 	
-	@After
-	public void teardown() {
-		mDriver.quit();
-	}
-
 }
