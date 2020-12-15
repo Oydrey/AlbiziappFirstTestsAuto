0f48829307c553c8097fa45980d8e5e0b01e31a0
Givranuke <43473001+Givranuke@users.noreply.github.com>
2020-12-15 13:24:34 +0100

    testfini

--- src/test/java/com/automation/selenium/matheo/CTRF121accedscore.java
+++ src/test/java/com/automation/selenium/matheo/CTRF121accedscore.java
@@ -1,12 +1,8 @@
-// verif fonction
-aa
 package com.automation.selenium.matheo;
 
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
-import org.junit.Assert;
-
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
@@ -28,7 +24,7 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.net.MalformedURLException;
 import java.net.URL;
-public class CTRF1272gagneuntrofeTest {
+public class CTRF121accedscore {
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
@@ -43,23 +39,12 @@ public class CTRF1272gagneuntrofeTest {
     driver.quit();
   }
   @Test
-  public void cTRF1272gagneuntrofe() throws InterruptedException {
+  public void cTRF121() {
 	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    driver.get("https://albiziapp.ozytis.fr/");
+	driver.get("https://albiziapp.ozytis.fr/");
+	FonctionMethods.login(driver,"tqlgroupe1@gmail.com" , "tqlgroupe1");
     driver.manage().window().setSize(new Dimension(785, 625));
-    FonctionMethods.login(driver,"tqlgroupe1@gmail.com" , "tqlgroupe1");
     driver.findElement(By.cssSelector(".fa-trophy")).click();
-    WebElement verif = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/ul/li[3]/div[1]/p"));
-    if (verif.getText() == "0 / 5") {
-    	driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/button[1]")).click();
-		for(int i = 1; i < 6; i++) { //on fait la promiere misson (5 relve)
-			FonctionMethods.testRenseignerReleveSurLaCarteSansPhoto(driver);
-          	System.out.println("Printing " + verif.getText() + i);
-		}
-		
-	}
-    driver.findElement(By.cssSelector(".fa-trophy")).click();
-    WebElement verif2 = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/ul/li[3]/div[1]/p"));
-    assertTrue(verif2.getText().matches("([1-5]) / 5"));
+	assertEquals("https://albiziapp.ozytis.fr/score", driver.getCurrentUrl());
   }
 }
