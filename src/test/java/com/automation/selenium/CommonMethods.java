7a5bfa33ffa70fe5c8ea486fd347f6ad7bc30978
Oydrey <oydrey@gmail.com>
2020-12-16 23:44:25 +0100

    add commons methods de releve

--- src/test/java/com/automation/selenium/CommonMethods.java
+++ src/test/java/com/automation/selenium/CommonMethods.java
@@ -47,12 +47,12 @@ public class CommonMethods {
 	}
 	
 	public static void testRenseignerReleveSurLaCarteAvecAjoutPhoto(WebDriver driver) throws InterruptedException {		
-		Thread.sleep(5000);
+		Thread.sleep(1000);
 		Actions builder = new Actions(driver);
 		builder.moveToElement(driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[1]")), 0, 0);
 		builder.moveByOffset(100, 100).click().build().perform();
 		
-		WebElement vCreatePlan = driver.findElement(ByXPath.xpath("/html/body/div[5]/div[3]/div/div[2]/button[2]"));
+		WebElement vCreatePlan = driver.findElement(ByXPath.xpath("/html/body/div[6]/div[3]/div/div[2]/button[2]"));
 		vCreatePlan.click();
 		
 		WebElement genreCommun = driver.findElement(By.id("commonGenusSelect"));
@@ -74,7 +74,159 @@ public class CommonMethods {
 		WebElement valider = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/button[1]"));
 		valider.click();
 		
-		WebElement confirmValider = driver.findElement(ByXPath.xpath("/html/body/div[5]/div[3]/div/div[2]/button[2]"));
+		Thread.sleep(2000);
+		
+		WebElement confirmValider = driver.findElement(ByXPath.xpath("/html/body/div[6]/div[3]/div/div[2]/button[2]"));
+		confirmValider.click();
+	}
+	
+	public static void testRenseignerReleveSurLaCarteSansAjoutPhoto(WebDriver driver) throws InterruptedException {
+		Thread.sleep(1000);
+		Actions builder = new Actions(driver);
+		builder.moveToElement(driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[1]")), 0, 0);
+		builder.moveByOffset(100, 100).click().build().perform();
+		
+		WebElement vCreatePlan = driver.findElement(ByXPath.xpath("/html/body/div[6]/div[3]/div/div[2]/button[2]"));
+		vCreatePlan.click();
+		
+		WebElement genreCommun = driver.findElement(By.id("commonGenusSelect"));
+		WebElement genreLatin = driver.findElement(By.id("GenusSelect"));
+		WebElement especeCommune = driver.findElement(By.id("commonSpeciesSelect"));
+		WebElement especeLatine = driver.findElement(By.id("SpeciesSelect"));
+		
+		genreCommun.sendKeys("Albizzia" + Keys.DOWN + Keys.ENTER);
+		genreLatin.sendKeys("Albizia" + Keys.DOWN + Keys.ENTER);
+		especeCommune.sendKeys("Albizzia commun" + Keys.DOWN + Keys.ENTER);
+		especeLatine.sendKeys("Albizia Julibrissin" + Keys.DOWN + Keys.ENTER);	
+		
+		WebElement confiant = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[5]/label[3]/span[1]"));
+		confiant.click();
+
+		WebElement valider = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/button[1]"));
+		valider.click();
+		
+		Thread.sleep(2000);
+		
+		WebElement confirmValider = driver.findElement(ByXPath.xpath("/html/body/div[6]/div[3]/div/div[2]/button[2]"));
+		confirmValider.click();
+	}
+	
+	public static void testRenseignerReleveSurLaCarteAvecUniquementLeGenreEtAjoutPhoto(WebDriver driver) throws InterruptedException {
+		Thread.sleep(1000);
+		Actions builder = new Actions(driver);
+		builder.moveToElement(driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[1]")), 0, 0);
+		builder.moveByOffset(100, 100).click().build().perform();
+		
+		WebElement vCreatePlan = driver.findElement(ByXPath.xpath("/html/body/div[6]/div[3]/div/div[2]/button[2]"));
+		vCreatePlan.click();
+		
+		WebElement genreCommun = driver.findElement(By.id("commonGenusSelect"));
+		WebElement genreLatin = driver.findElement(By.id("GenusSelect"));
+		
+		genreCommun.sendKeys("Albizzia" + Keys.DOWN + Keys.ENTER);
+		genreLatin.sendKeys("Albizia" + Keys.DOWN + Keys.ENTER);
+		
+		WebElement confiant = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[5]/label[3]/span[1]"));
+		confiant.click();
+		
+		WebElement inputPicture = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[6]/div/div/input"));
+		inputPicture.sendKeys("C:/Users/Oydrey/Pictures/arbre.jpg");
+
+		WebElement valider = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/button[1]"));
+		valider.click();
+		
+		Thread.sleep(2000);
+		
+		WebElement confirmValider = driver.findElement(ByXPath.xpath("/html/body/div[6]/div[3]/div/div[2]/button[2]"));
+		confirmValider.click();
+	}
+	
+	public static void testRenseignerReleveSurLaCarteAvecUniquementLeGenreSansAjoutPhoto(WebDriver driver) throws InterruptedException {
+		Thread.sleep(1000);
+		Actions builder = new Actions(driver);
+		builder.moveToElement(driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[1]")), 0, 0);
+		builder.moveByOffset(100, 100).click().build().perform();
+		
+		WebElement vCreatePlan = driver.findElement(ByXPath.xpath("/html/body/div[6]/div[3]/div/div[2]/button[2]"));
+		vCreatePlan.click();
+		
+		WebElement genreCommun = driver.findElement(By.id("commonGenusSelect"));
+		WebElement genreLatin = driver.findElement(By.id("GenusSelect"));
+		
+		genreCommun.sendKeys("Albizzia" + Keys.DOWN + Keys.ENTER);
+		genreLatin.sendKeys("Albizia" + Keys.DOWN + Keys.ENTER);
+		
+		WebElement confiant = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[5]/label[3]/span[1]"));
+		confiant.click();
+
+		WebElement valider = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/button[1]"));
+		valider.click();
+		
+		Thread.sleep(2000);
+		
+		WebElement confirmValider = driver.findElement(ByXPath.xpath("/html/body/div[6]/div[3]/div/div[2]/button[2]"));
+		confirmValider.click();
+	}
+	
+	public static void testRenseignerReleveSurLaCarteAvecPeuConfiant(WebDriver driver) throws InterruptedException {
+		Thread.sleep(1000);
+		Actions builder = new Actions(driver);
+		builder.moveToElement(driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[1]")), 0, 0);
+		builder.moveByOffset(100, 100).click().build().perform();
+		
+		WebElement vCreatePlan = driver.findElement(ByXPath.xpath("/html/body/div[6]/div[3]/div/div[2]/button[2]"));
+		vCreatePlan.click();
+		
+		WebElement genreCommun = driver.findElement(By.id("commonGenusSelect"));
+		WebElement genreLatin = driver.findElement(By.id("GenusSelect"));
+		WebElement especeCommune = driver.findElement(By.id("commonSpeciesSelect"));
+		WebElement especeLatine = driver.findElement(By.id("SpeciesSelect"));
+		
+		genreCommun.sendKeys("Albizzia" + Keys.DOWN + Keys.ENTER);
+		genreLatin.sendKeys("Albizia" + Keys.DOWN + Keys.ENTER);
+		especeCommune.sendKeys("Albizzia commun" + Keys.DOWN + Keys.ENTER);
+		especeLatine.sendKeys("Albizia Julibrissin" + Keys.DOWN + Keys.ENTER);	
+		
+		WebElement peuConfiant = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[5]/label[1]/span[1]"));
+		peuConfiant.click();
+
+		WebElement valider = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/button[1]"));
+		valider.click();
+		
+		Thread.sleep(2000);
+		
+		WebElement confirmValider = driver.findElement(ByXPath.xpath("/html/body/div[6]/div[3]/div/div[2]/button[2]"));
+		confirmValider.click();
+	}
+	
+	public static void testRenseignerReleveSurLaCarteAvecMoyennementConfiant(WebDriver driver) throws InterruptedException {
+		Thread.sleep(1000);
+		Actions builder = new Actions(driver);
+		builder.moveToElement(driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[1]")), 0, 0);
+		builder.moveByOffset(100, 100).click().build().perform();
+		
+		WebElement vCreatePlan = driver.findElement(ByXPath.xpath("/html/body/div[6]/div[3]/div/div[2]/button[2]"));
+		vCreatePlan.click();
+		
+		WebElement genreCommun = driver.findElement(By.id("commonGenusSelect"));
+		WebElement genreLatin = driver.findElement(By.id("GenusSelect"));
+		WebElement especeCommune = driver.findElement(By.id("commonSpeciesSelect"));
+		WebElement especeLatine = driver.findElement(By.id("SpeciesSelect"));
+		
+		genreCommun.sendKeys("Albizzia" + Keys.DOWN + Keys.ENTER);
+		genreLatin.sendKeys("Albizia" + Keys.DOWN + Keys.ENTER);
+		especeCommune.sendKeys("Albizzia commun" + Keys.DOWN + Keys.ENTER);
+		especeLatine.sendKeys("Albizia Julibrissin" + Keys.DOWN + Keys.ENTER);	
+		
+		WebElement moyennementConfiant = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[5]/label[2]/span[1]"));
+		moyennementConfiant.click();
+
+		WebElement valider = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/button[1]"));
+		valider.click();
+		
+		Thread.sleep(2000);
+		
+		WebElement confirmValider = driver.findElement(ByXPath.xpath("/html/body/div[6]/div[3]/div/div[2]/button[2]"));
 		confirmValider.click();
 	}
 	
---
+++ src/test/java/com/automation/selenium/goran/TestVoirNombreIdentificactions.java
@@ -0,0 +1,65 @@
+package com.automation.selenium.goran;
+
+import static org.junit.Assert.assertTrue;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.automation.selenium.CommonMethods;
+
+public class TestVoirNombreIdentificactions {
+
+	private static final DesiredCapabilities CAPABILITY = DesiredCapabilities.chrome();
+
+	private static final String SELENIUM_SERVER_URL = "http://127.0.0.1:4444/wd/hub";
+
+	private static final String LOGIN_URL = "https://albiziapp.ozytis.fr/login";
+	
+	private WebDriver driver = null;
+	
+	private String email = "goran.sarr@gmail.com";
+	
+	private String password = "*Oxana36*";
+	
+	@Before
+	public void setup() throws MalformedURLException, InterruptedException {
+		// Create a new instance of the driver
+		driver = new RemoteWebDriver(new URL(SELENIUM_SERVER_URL), CAPABILITY);
+		
+		// And now use this to open base url
+		driver.navigate().to(LOGIN_URL);
+
+		// Ask to the driver to wait for 3s when an element is not found
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void test() throws InterruptedException {
+		CommonMethods.login(driver, email, password);
+		driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[2]/button[4]/span/span[1]")).click();
+		String text = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[2]/div[4]/button/div[2]/p")).getText();
+		int nombreIdentificationAvantReleve = Integer.valueOf(text.split(" ")[3]);
+		driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[2]/button[1]")).click();
+		CommonMethods.testRenseignerReleveSurLaCarteAvecAjoutPhoto(driver);
+		driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[2]/button[4]/span/span[1]")).click();
+		text = driver.findElement(ByXPath.xpath("//*[@id=\"root\"]/div/div[1]/div[2]/div[4]/button/div[2]/p")).getText();
+		int nombreIdentificationApresReleve = Integer.valueOf(text.split(" ")[3]);
+		assertTrue((nombreIdentificationAvantReleve + 1)==nombreIdentificationApresReleve);
+	}
+	
+	@After
+	public void teardown() {
+		driver.quit();
+	}
+	
+}
