1f2554ed93e7cb87dbb4a65784305bbb6f051948
manalbou <40423479+manalbou@users.noreply.github.com>
2020-12-16 15:33:56 +0100

    Update CTRF10231x2Naviguerentrelesphotos.java

--- src/test/java/com/automation/selenium/manal/CTRF10231x2Naviguerentrelesphotos.java
+++ src/test/java/com/automation/selenium/manal/CTRF10231x2Naviguerentrelesphotos.java
@@ -49,9 +49,11 @@ public class CTRF10231x2Naviguerentrelesphotos {
     driver.manage().window().setSize(new Dimension(1050, 670));
     driver.findElement(By.cssSelector(".MuiIconButton-label > .MuiSvgIcon-root")).click();
     driver.findElement(By.cssSelector(".MuiButtonBase-root:nth-child(2) .MuiTypography-root")).click();
+    Thread.sleep(5000);
     driver.findElement(By.cssSelector(".MuiInputBase-input")).click();
     driver.findElement(By.cssSelector(".MuiInputBase-input")).sendKeys("chene vert");
     driver.findElement(By.cssSelector(".MuiListItemText-primary")).click();
+    Thread.sleep(5000);
   }
 }
 
