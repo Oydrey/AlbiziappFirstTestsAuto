8bee3bd3ec60add9ceb728a2e52819d3e2ae89a5
Oydrey <oydrey@gmail.com>
2021-01-22 09:45:21 +0100

    test junit suitcase

---
+++ src/test/java/com/automation/selenium/AllTests.java
@@ -0,0 +1,11 @@
+package com.automation.selenium;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AlbiziappTest.class, TestsCommonMethods.class })
+public class AllTests {
+
+}
