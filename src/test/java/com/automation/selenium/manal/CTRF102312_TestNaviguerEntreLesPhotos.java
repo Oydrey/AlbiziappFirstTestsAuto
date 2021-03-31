6f3defd0f2d66ae876766a11c1e50d46314f28bb
manalbou <40423479+manalbou@users.noreply.github.com>
2021-03-31 16:38:28 +0200

    Update CTRF102312_TestNaviguerEntreLesPhotos.java

--- src/test/java/com/automation/selenium/manal/CTRF102312_TestNaviguerEntreLesPhotos.java
+++ src/test/java/com/automation/selenium/manal/CTRF102312_TestNaviguerEntreLesPhotos.java
@@ -7,18 +7,25 @@ import org.openqa.selenium.Dimension;
 import com.automation.selenium.CommonMethods;
 import com.automation.selenium.Constantes;
 
-public class CTRF102312_TestNaviguerEntreLesPhotos {
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CTRF102312_TestNaviguerEntreLesPhotos throws InterruptedException {
 	
 	public static void test(WebDriver driver) throws InterruptedException{
 		CommonMethods.login(driver, Constantes.LOGIN_TQL_EMAIL, Constantes.LOGIN_TQL_PASSWORD);
 		driver.manage().window().setSize(new Dimension(1050, 670));
-		driver.findElement(By.cssSelector(".MuiIconButton-label > .MuiSvgIcon-root")).click();
-		driver.findElement(By.cssSelector(".MuiButtonBase-root:nth-child(2) .MuiTypography-root")).click();
-		Thread.sleep(5000);
-		driver.findElement(By.cssSelector(".MuiInputBase-input")).click();
-		driver.findElement(By.cssSelector(".MuiInputBase-input")).sendKeys("chene vert");
-		driver.findElement(By.cssSelector(".MuiListItemText-primary")).click();
-		Thread.sleep(5000);
+		 driver.findElement(By.xpath("//*[@id=\"root\"]/div/header/div/button")).click();
+    		driver.findElement(By.xpath("//span[contains(.,'Flore')]")).click();
+    		driver.findElement(By.cssSelector(".MuiInputBase-input")).sendKeys("chene vert");
+    		Thread.sleep(2000);
+    		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/ul/div/div[1]")).click();
+    		driver.findElement(By.xpath("//div[@id=\'root\']/div/div/div/div[2]/span[3]")).click();
+    		Thread.sleep(2000);
+    		driver.findElement(By.xpath("//div[@id=\'root\']/div/div/div/div[2]/span[4]")).click();
+    		driver.findElement(By.xpath("//div[@id=\'root\']/div/div/div/div[2]/span[5]")).click();
+    		Thread.sleep(2000);
 	}
 }
 
