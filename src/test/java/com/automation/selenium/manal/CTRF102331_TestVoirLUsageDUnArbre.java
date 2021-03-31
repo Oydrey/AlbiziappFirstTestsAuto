03ee27be47c65e6718ea07fbf5bbb1d0433a38a4
manalbou <40423479+manalbou@users.noreply.github.com>
2021-03-31 16:54:29 +0200

    Update CTRF102331_TestVoirLUsageDUnArbre.java

--- src/test/java/com/automation/selenium/manal/CTRF102331_TestVoirLUsageDUnArbre.java
+++ src/test/java/com/automation/selenium/manal/CTRF102331_TestVoirLUsageDUnArbre.java
@@ -12,15 +12,15 @@ public class CTRF102331_TestVoirLUsageDUnArbre {
 
 	 public static void test(WebDriver driver) throws InterruptedException {
 		CommonMethods.login(driver, Constantes.LOGIN_TQL_EMAIL, Constantes.LOGIN_TQL_PASSWORD);
-	    driver.findElement(By.xpath("//*[@id=\"root\"]/div/header/div/button")).click();
-	    Thread.sleep(2000);
-		driver.findElement(By.xpath("/html/body/div[6]/div[3]/ul/div[1]")).click();
-		driver.navigate().refresh();
+		driver.manage().window().setSize(new Dimension(1050, 670));
+	        driver.findElement(By.xpath("//*[@id=\"root\"]/div/header/div/button")).click();
+		driver.findElement(By.xpath("//span[contains(., 'Flore')]")).click();
 		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/div/div/input")).sendKeys("chene vert");
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/ul/div/div[1]")).click();
 		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div[2]/div/div/button[2]/span[1]")).click();
-	    Thread.sleep(5000);
-	    String usageText = "L'écorce se caractérise par une forte richesse en tanins. Elle était récoltée pour la tannerie. Le chêne vert a été une source de bois de chauffage depuis l'Antiquité.\r\n"
+    		Thread.sleep(5000);
+	    	String usageText = "L'écorce se caractérise par une forte richesse en tanins. Elle était récoltée pour la tannerie. Le chêne vert a été une source de bois de chauffage depuis l'Antiquité.\r\n"
 	    		+ "\r\n"
 	    		+ "Ses fuits, les glands, servent également de nourriture pour le bétail en élevage extensif.";
 		assertEquals(usageText, driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div[3]/p[2]")).getText());
