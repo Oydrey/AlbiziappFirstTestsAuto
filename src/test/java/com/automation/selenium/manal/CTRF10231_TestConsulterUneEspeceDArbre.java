51e194a2c68f8d06f367caba40709c7c83ada8cd
manalbou <40423479+manalbou@users.noreply.github.com>
2021-03-31 16:44:54 +0200

    Update CTRF10231_TestConsulterUneEspeceDArbre.java

--- src/test/java/com/automation/selenium/manal/CTRF10231_TestConsulterUneEspeceDArbre.java
+++ src/test/java/com/automation/selenium/manal/CTRF10231_TestConsulterUneEspeceDArbre.java
@@ -7,19 +7,20 @@ import org.openqa.selenium.Dimension;
 import com.automation.selenium.CommonMethods;
 import com.automation.selenium.Constantes;
 
-public class CTRF10231_TestConsulterUneEspeceDArbre {
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CTRF10231_TestConsulterUneEspeceDArbre throws InterruptedException{
 	
   public static void test(WebDriver driver) throws InterruptedException {
     CommonMethods.login(driver, Constantes.LOGIN_TQL_EMAIL, Constantes.LOGIN_TQL_PASSWORD);
     driver.manage().window().setSize(new Dimension(1050, 670));
-    driver.findElement(By.cssSelector(".MuiIconButton-label > .MuiSvgIcon-root")).click();
-    driver.findElement(By.cssSelector(".MuiButtonBase-root:nth-child(2) > .MuiListItemIcon-root")).click();
-    Thread.sleep(5000);
-    driver.findElement(By.cssSelector(".MuiInputBase-input")).click();
-    Thread.sleep(5000);
+    driver.findElement(By.xpath("//*[@id=\"root\"]/div/header/div/button")).click();
+    driver.findElement(By.xpath("//span[contains(., 'Flore')]")).click();
     driver.findElement(By.cssSelector(".MuiInputBase-input")).sendKeys("chene vert");
-    Thread.sleep(5000);
-    driver.findElement(By.cssSelector(".MuiListItemText-primary")).click();
+    Thread.sleep(2000);
+    driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/ul/div/div[1]")).click();
     Thread.sleep(5000);
   }
   
