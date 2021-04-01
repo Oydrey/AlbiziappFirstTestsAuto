a67df773581dc0b08fc2158217b785a20b74aaab
manalbou <40423479+manalbou@users.noreply.github.com>
2021-04-01 10:27:51 +0200

    Update CTRF141_TestReprendreMissionEnCours.java

--- src/test/java/com/automation/selenium/manal/CTRF141_TestReprendreMissionEnCours.java
+++ src/test/java/com/automation/selenium/manal/CTRF141_TestReprendreMissionEnCours.java
@@ -5,32 +5,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import com.automation.selenium.CommonMethods;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CTRF141_TestReprendreMissionEnCours {
 	
-	public static void test(WebDriver driver) {
+	public static void test(WebDriver driver) throws InterruptedException {
+		CommonMethods.login(driver, Constantes.LOGIN_OYDREY_EMAIL, Constantes.LOGIN_OYDREY_PASSWORD);
 		driver.manage().window().setSize(new Dimension(1050, 670));
-		driver.findElement(By.cssSelector(".MuiIconButton-label path")).click();
-		
-		{
-			WebElement element = driver.findElement(By.cssSelector(".MuiIconButton-label path"));
-			Actions builder = new Actions(driver);
-			builder.moveToElement(element).perform();
-		}
-		
-		{
-			WebElement element = driver.findElement(By.tagName("body"));
-			Actions builder = new Actions(driver);
-			builder.moveToElement(element, 0, 0).perform();
-		}
-		
-		driver.findElement(By.cssSelector(".MuiButtonBase-root:nth-child(2) > .MuiListItemIcon-root")).click();
-		driver.findElement(By.cssSelector(".MuiInputBase-input")).click();
-		driver.findElement(By.cssSelector(".MuiInputBase-input")).sendKeys("ulex");
-		driver.findElement(By.cssSelector(".MuiListItemText-primary")).click();
-		driver.findElement(By.cssSelector(".material-icons:nth-child(2)")).click();
-		driver.findElement(By.cssSelector(".material-icons:nth-child(3)")).click();
-		driver.findElement(By.cssSelector(".material-icons:nth-child(4)")).click();
-		driver.findElement(By.cssSelector(".material-icons:nth-child(5)")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.cssSelector(".MuiIconButton-label > .MuiSvgIcon-root")).click();
+		Thread.sleep(2000);
+		CommonMethods.testSeDeconnecter(driver);
+	        CommonMethods.login(driver, Constantes.LOGIN_OYDREY_EMAIL, Constantes.LOGIN_OYDREY_PASSWORD);
+		Thread.sleep(5000);
+		driver.findElement(By.cssSelector(".MuiIconButton-label > .MuiSvgIcon-root")).click();
+		Thread.sleep(5000);
 	}
 }
